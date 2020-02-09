785803c4ca600298406a0ebb57f164b6a3632d9f
gabriel.salomon <gabriel.salomon.2007@gmail.com>
2020-02-09 10:19:28 -0600

    the tutorial 1 was added.

---
+++ Tutorial1/src/com/company/Variables.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        int age = 67;
+        String name = "Jane";
+
+        double distance = 45.6;
+
+        System.out.println("HEllO WORLD");
+        System.out.println("name");
+
+        System.out.println(name + " is " + age + " years old.");
+
+        System.out.println(name + " walked " + distance + " meters.");
+    }
+
+}
