6a76b1edb1b1dc9c336719c7ecb4f1ed642bccb5
gabriel.salomon <gabriel.salomon.2007@gmail.com>
2020-02-09 10:53:07 -0600

    This is a project i made with Variables

---
+++ Variables Challenge/src/com/company/Variables.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Variables {
+    public static void main(String[] args) {
+        double inchVal = 20000;
+        double meterVal = inchVal * 0.0254;
+        System.out.println(inchVal + " inches is " + meterVal + " meters.");
+    }
+
+}
